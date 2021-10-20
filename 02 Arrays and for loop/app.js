370f70732db8277eadd2bba2a0b2fa5a22a05d60
Kamil Grabiec <kamil.grabiec@pl.abb.com>
2021-10-20 16:01:38 +0200

    'cd'

---
+++ 02 Arrays and for loop/app.js
@@ -0,0 +1,33 @@
+// Arrays and for loop
+
+const names = ['Anna', 'Susy', 'Bob'];
+const lastName = 'Shakeandbake';
+let fullNames = [];
+
+//for loop
+for (let i = 0; i < names.length; i++) {
+  //fullNames[i] = names[i] + ' ' + lastName;
+  fullNames.push(`${names[i]} ${lastName}`);
+  console.log(fullNames[i]);
+}
+
+//Functions, return, if, arrays, for loop
+
+const gas = [20, 40, 100, 110];
+const food = [10, 30, 50];
+
+function calculateTotal(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}
+
+const gasTotal = calculateTotal(gas);
+const foodTotal = calculateTotal(food);
+
+console.log({
+  gas: gasTotal,
+  food: foodTotal,
+});
