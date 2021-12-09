fbf9949bb8b73e8e149f7e55aa7cf08980671634
Kamil Grabiec <kamilgrabiec@interia.pl>
2021-12-09 12:03:59 +0100

    'cd'

--- 04 Scope/app.js
+++ 04 Scope/app.js
@@ -3,13 +3,13 @@
 // Any variable available outside code block {} is called Global Scope
 // can be access anywhere in the program
 
-let name1 = 'Bobo'; // let declare variable as global
-name1 = 'Peter';
+let name1 = "Bobo"; // let declare variable as global
+name1 = "Peter";
 
 function calculate() {
   // some other code ...
   console.log(name1);
-  name1 = 'orange';
+  name1 = "orange";
 }
 
 calculate();
@@ -17,7 +17,7 @@ console.log(`my name is ${name1} and I'm awesome`);
 
 if (true) {
   console.log(name1);
-  name1 = 'pants';
+  name1 = "pants";
 }
 
 console.log(`my name is ${name1} and I'm awesome`);
@@ -25,18 +25,18 @@ console.log(`my name is ${name1} and I'm awesome`);
 // local scope
 // available only withing code block {}
 
-let name2 = 'Peter';
+let name2 = "Peter";
 
 function calculate2() {
-  let name3 = 'Apple';
-  const name2 = 'Bryan';
+  let name3 = "Apple";
+  const name2 = "Bryan";
   console.log(name2);
-  becomesGlobal = 'global variable'; // because we have not use key word (const/let)
+  becomesGlobal = "global variable"; // because we have not use key word (const/let)
 }
 
-calculate2();
-console.log(name2);
-console.log(becomesGlobal);
+calculate2(); //Display 'Bryan' because has access to local variable name2
+console.log(name2); //Even 'calculate2() function displayed 'Bryan' console.log display 'Peter' because it does not have access to local instance of name2 but use instead global instance of name2
+console.log(becomesGlobal); //However, becomesGlobal is global because 1) calculate2() code was already executed 2) becomesGlobal does not have key word so it is treated as global
 //console.log(name3);
 
 // Variable lookup
@@ -58,5 +58,5 @@ function add(num1, num2) {
   multiply();
   return result;
 }
-
+//JS takes the most possible local version of variable so globalNumber is 10 because it is declared inside function (local scope)
 console.log(add(3, 4));
