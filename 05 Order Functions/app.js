190b72e2165bcb83b0fb539f53feb609931f70ee
Kamil Grabiec <kamil.grabiec@pl.abb.com>
2021-12-21 14:44:01 +0100

    Order Functions

--- 05 Order Functions/app.js
+++ 05 Order Functions/app.js
@@ -48,9 +48,10 @@ console.log(`Next chapter`);
 
 const number = [0, 1, 2, 3, 4];
 const people = [
-  { name: 'bob', age: 20, position: 'developer' },
-  { name: 'suzy', age: 30, position: 'the boss' },
-  { name: 'peter', age: 47, position: 'designer' },
+  { name: 'bob', age: 20, position: 'developer', id: 1 },
+  { name: 'suzy', age: 30, position: 'the boss', id: 2 },
+  { name: 'peter', age: 47, position: 'designer', id: 3 },
+  { name: `anna`, age: 32, position: `QA`, id: 4 },
 ];
 //show all numbers
 
@@ -71,3 +72,102 @@ console.log(`forEach with annonymus function`);
 people.forEach(function (item) {
   console.log(item.position.toUpperCase());
 });
+
+//map method - it does return new array the same size as original array
+console.log(`Map method`);
+
+const ages = people.map(function (person) {
+  return person.age + 20;
+});
+
+const newPeople = people.map(function (person) {
+  return {
+    firstName: person.name.toUpperCase(),
+    oldAge: person.age + 15,
+    newPosition: person.position,
+    newId: person.id,
+  };
+});
+console.log(ages);
+console.log(newPeople);
+
+const names = people.map(function (person) {
+  return `<h2>${person.name}</h2>`;
+});
+
+document.body.innerHTML = names.join('');
+
+console.log(names);
+
+console.log(`Next chapter - Fliter`);
+//Filter
+// dose return new array
+// can manipulate the size of new array
+// returns based on condition
+
+const youngPeople = people.filter(function (person) {
+  return person.age <= 30;
+});
+
+const developers = people.filter(function (person) {
+  return person.position === `developer`;
+});
+
+console.log(youngPeople);
+console.log(developers);
+
+console.log(`Next chapter - find`);
+// Find
+// returns single instance (in this case object)
+// returns first match, if no match then undefined
+// great for getting unique value
+
+const person = people.find(function (person) {
+  return person.id === 2;
+});
+
+console.log(person);
+console.log(
+  people.find(function (person) {
+    return person.name === 'bob';
+  })
+);
+console.log(
+  people.find(function (person) {
+    return person.name === 'bob';
+  }).name
+);
+
+//Difference between find and filter. Filter returns array so it is necessary to first point which element of array we access by [i]
+
+const person2 = people.filter(function (person) {
+  return person.id === 2;
+});
+
+console.log(person2[0].name);
+
+console.log(`Next chapter - reduce`);
+//Reduce
+// iterates over array, use callback function - as previous
+// reduce to a single value, number, array, object
+// 1 parameter ('acc') - total of all calculations
+// 2 parameter ('curr') - current iteration/value
+
+const people3 = [
+  { name: 'bob', age: 20, position: 'developer', id: 1, salary: 200 },
+  { name: 'suzy', age: 30, position: 'the boss', id: 2, salary: 300 },
+  { name: 'peter', age: 47, position: 'designer', id: 3, salary: 500 },
+  { name: `anna`, age: 32, position: `QA`, id: 4, salary: 500 },
+];
+
+const total = people3.reduce(function (acc, currItem) {
+  console.log(acc);
+  console.log(`Total ${acc}`);
+  console.log(currItem);
+  console.log(`Current money: ${currItem.salary}`);
+  acc += currItem.salary;
+
+  return acc;
+}, 1000);
+
+console.log(total);
